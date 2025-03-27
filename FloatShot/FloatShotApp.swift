59a045c58e4387b88f3b691ab3eacf3bf191d89f
Ryan Morrison <ryan.morrison04@icloud.com>
2025-03-27 13:19:19 -0400

    Initial Commit

---
+++ FloatShot/FloatShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatShotApp.swift
+//  FloatShot
+//
+//  Created by Ryan Morrison on 2025-03-27.
+//
+
+import SwiftUI
+
+@main
+struct FloatShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatShotTests/FloatShotTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatShotTests.swift
+//  FloatShotTests
+//
+//  Created by Ryan Morrison on 2025-03-27.
+//
+
+import Testing
+@testable import FloatShot
+
+struct FloatShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
