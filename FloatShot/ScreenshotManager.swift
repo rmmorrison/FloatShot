ba835d4b385f6b16a7f8fff51a4644895baa074d
Ryan Morrison <ryan.morrison04@icloud.com>
2025-03-29 18:52:51 -0400

    Fix low-resolution or 'blurry' screenshots

--- FloatShot/ScreenshotManager.swift
+++ FloatShot/ScreenshotManager.swift
@@ -42,11 +42,13 @@ class ScreenshotManager {
 
     @MainActor
     func captureImage(screen: NSScreen, rect: CGRect) async throws -> CGImage? {
+        let scale = screen.backingScaleFactor
+
         let config = SCStreamConfiguration()
-        config.sourceRect = rect
-        config.width = Int(rect.width)
-        config.height = Int(rect.height)
-        
+        config.capturesAudio = false
+        config.width = Int(screen.frame.width * scale)
+        config.height = Int(screen.frame.height * scale)
+
         guard let screenDisplayID = screen.displayID else {
             return nil
         }
@@ -57,7 +59,32 @@ class ScreenshotManager {
         }
 
         let filter = SCContentFilter(display: display, excludingWindows: [])
-        return try await SCScreenshotManager.captureImage(contentFilter: filter, configuration: config)
+
+        // capture a full image of the screen.
+        // while SCStreamConfiguration allows us to set a 'sourceRect' of the area to capture,
+        // it does so producing what appears to be a low resolution or 'blurry' screenshot.
+        // we mitigate this by not defining a 'sourceRect' here and instead crop later
+        let fullImage = try await SCScreenshotManager.captureImage(contentFilter: filter, configuration: config)
+
+        // convert source rectangle which was measured in points to pixels
+        // multiply by display scale factor
+        let scaledRect = CGRect(
+            x: rect.origin.x * scale,
+            y: rect.origin.y * scale,
+            width: rect.width * scale,
+            height: rect.height * scale
+        )
+
+        // flip the y-axis - NSScreen.frame.origin and CGImage pixel data start in opposite locations
+        let imageHeight = fullImage.height
+        let flippedRect = CGRect(
+            x: scaledRect.origin.x,
+            y: CGFloat(imageHeight) - scaledRect.origin.y - scaledRect.height,
+            width: scaledRect.width,
+            height: scaledRect.height
+        )
+
+        return fullImage.cropping(to: flippedRect)
     }
 
     func forcePermissionRequest() async throws {
